f056f2c3e2def884513978e5c51641111353c7b8
fade <3487141998@qq.com>
2022-12-04 21:12:43 +0800

    5

--- WOrkOne/MyfuntionsOne.py
+++ WOrkOne/MyfuntionsOne.py
@@ -4,32 +4,33 @@ headline = []
 
 def load_info():
     """从文件导入学生信息"""
-    fr = open("student.txt", 'r',encoding="UTF-8")
+    fr = open("student.txt", 'r', encoding="UTF-8")
     head = fr.readline()
     headline.append(head)  # 为后面写入表头做准备
     fr.seek(0, 0)
     lines = fr.readlines()
-    field = lines.pop(0).split(',')  # 移除表头
+    field = lines.pop(0).split('\t')  # 移除表头
     for eachline in lines:
         t = eachline.split('\t')  # 取出每行数据，即列表 lines 中的一个元素
-    t[7] = t[7].strip('\n')  # 删除每行后的换行符
-    card_dict = {
-        'num_str': t[0],
-        'name_str': t[1],
-        'class_str': t[2],
-        'sex_str': t[3],
-        'age_str': t[4],
-        'phone_str': t[5],
-        'qq_str': t[6],
-        'addr_str': t[7]}
-    card_list.append(card_dict)  # 将学生信息字典添加到列表中
+        # print(f"t={t[2]}")
+        t[7] = t[7].strip('\n')  # 删除每行后的换行符
+        card_dict = {
+            'num_str': t[0],
+            'name_str': t[1],
+            'class_str': t[2],
+            'sex_str': t[3],
+            'age_str': t[4],
+            'phone_str': t[5],
+            'qq_str': t[6],
+            'addr_str': t[7]}
+        card_list.append(card_dict)  # 将学生信息字典添加到列表中
     print('学生信息成功导入！')
     fr.close()
     pass
 
 
 def save_info():
-    fw = open("student.txt", 'w', encoding="UTF-8")
+    fw = open("student.txt", 'w')
     fw.write(" ".join(headline))  # 写入表头
     for i in range(len(card_list)):
         fw.write(card_list[i]["num_str"] + "\t")  # 写入每行有效数据
