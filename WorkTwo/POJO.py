ce20dda4115f9244ba2b2709e3153c02b630b9d3
奇点 <3099234061@qq.com>
2022-12-08 18:40:48 +0800

    POJO compete

---
+++ WorkTwo/POJO.py
@@ -0,0 +1,15 @@
+class Stu:
+    def __init__(self, stuid, sex, name, classname, math, chinese, english):
+        self.StuId = stuid
+        self.ClassName = classname
+        self.Name = name
+        self.Math = math
+        self.Chinese = chinese
+        self.English = english
+        self.Sex = sex
+
+
+class User:
+    def __init__(self, pwd, username):
+        self.Password = pwd
+        self.Username = username
