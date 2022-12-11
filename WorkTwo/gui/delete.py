ad7c1a64cb5fc5b3c30952e22e23be3e4f1edc3d
wujihui <1578347002@qq.com>
2022-12-11 19:05:36 +0800

    update selectone delete second commit

--- WorkTwo/gui/delete.py
+++ WorkTwo/gui/delete.py
@@ -1,53 +1,60 @@
 import tkinter as tk
 from tkinter.messagebox import *
 from WorkTwo.data import *
+
+
+
 def check(id):
     if dao.delete(id):
         return True
     return False
 
 
+def delete(root,page):
 
-class Login(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title('删除')
-        self.geometry("500x200+500+300")  #窗口大小
-        self.setup_UI()
+    root.title('删除')
+    root.geometry("500x200+500+300")  #窗口大小
+    setup_UI(root,page)
 
-    def setup_UI(self):
-        rows = []
-        for i in range(3):
-            rows.append(tk.Frame(self))
-            rows[i].pack(fill="x",ipadx=1,ipady=2)
-        tk.Label(rows[0],text='请输入要删除的id号',font=10,width=20,pady=10).pack(side=tk.TOP)
-        self.id = tk.StringVar()
-        self.entid = tk.Entry(rows[0],textvariable=self.id,width=15,font=",'font=10',")
-        self.entid.pack(side=tk.TOP)
-        self.status=tk.StringVar()
-        self.statusLabel = tk.Label(rows[2],text='',fg='red',textvariable=self.status,width=36)
-        self.statusLabel.pack(side=tk.TOP)
-        tk.Button(rows[2],text="删除",font=8,width=10,command=self.fetch).pack(side=tk.TOP,ipadx=2)
-        tk.Button(rows[2],text="返回",font=8,width=10,command=self.cancel).pack(side=tk.TOP,ipadx=2)
+def setup_UI(root,page):
+    page.pack()
+    tk.Label(page,text='请输入要删除的id号',font=10,width=20,pady=10).pack(side=tk.TOP)
+    ids = tk.StringVar()
 
-    def fetch(self):
-        ans = askokcancel('Verify delete', "确定删除?")
-        if ans:
-            self.ok()
-    def ok(self):
-        self.login(self.id.get())
-    def login(self,id):
-        if id :
-            validate = check(id)
-            if validate:
-                self.status.set("删除成功")
-            else:
-                self.status.set('学号不存在')
+    entrys = tk.Entry(page, textvariable=ids,font=(",''20',"),width=15)
+    entrys.pack(side=tk.TOP)
+    global status
+    status=tk.StringVar()
+    statusLabel = tk.Label(page,text='',fg='red',textvariable=status,width=36)
+    statusLabel.pack(side=tk.TOP)
+    tk.Button(page,text="删除",font=8,width=10,command=lambda :fetch(ids)).pack(side=tk.TOP,ipadx=2)
+    tk.Button(page,text="返回",font=8,width=10,command=cancel).pack(side=tk.TOP,ipadx=2)
+    page.mainloop()
+
+def fetch(ids):
+    id = ids.get()
+    ans = askokcancel('Verify delete', "确定删除?")
+    if ans:
+        login( id)
+
+def login(id):
+
+    if id :
+        validate = check(id)
+        if validate:
+            status.set("删除成功")
         else:
-            self.status.set("请输入id号")
+            status.set('学号不存在')
+    else:
+        status.set("请输入id号")
 
-    def cancel(self):
-        pass
 
-if __name__ == '__main__':
-    Login().mainloop()
+def cancel():
+    pass
+
+def dels():
+
+    root = tk.Tk()
+    page = tk.Frame(root)
+
+    delete(root,page)
--- WorkTwo/gui/update.py
+++ WorkTwo/gui/update.py
@@ -11,117 +11,120 @@ def check(id) :
 
 
 
-class Login(tk.Tk):
-
-    def __init__(self):
-        super().__init__()
-        self.title('修改')
-        self.geometry("500x200+500+300")  # 窗口大小
-        self.setup_UI()
 
 
-    def setup_UI(self):
-        page = tk.Frame(self)
-        page.pack()
-        tk.Label(page, text='请输入要查询的id号: ', font=20, width=20, pady=15).grid(row=1, column=1)
-        self.id = tk.StringVar()
-        self.entid = tk.Entry(page, textvariable=self.id,font=(",''20',") , width=25)
-        self.id.set("1")
-        self.entid.grid(row=1, column=2)
-        self.status = tk.StringVar()
-        self.statusLabel = tk.Label(page, text='', font=15, fg='red', textvariable=self.status, width=10)
-        self.statusLabel.grid(row=2, column=1, columnspan=2)
-        tk.Button(page, text="返回", font=20, width=6, command=self.cancel).grid(row=4, column=2, pady=10,sticky=tk.N)
-        tk.Button(page, text="查询", font=20, width=6, command=self.ok).grid(row=4, column=1, pady=10, sticky=tk.N)
+def function(root,page):
+
+    root.title('修改')
+    root.geometry("500x200+500+300")  # 窗口大小
+
+    setup_UI(root,page)
 
 
-    def ok(self):
-        self.login(self.id.get())
-    def login(self,id):
-        if id :
-            validate = check(id)
-            if len(validate) != 0:
-                self.status.set("查找成功")
-                print(validate)
-                self.setup_UI2(validate)
-            else:
-                self.status.set('学号不存在')
+def setup_UI(root,page):
+    page.pack()
+    tk.Label(page, text='请输入要查询的id号: ', font=20, width=20, pady=15).grid(row=1, column=1)
+    id = tk.StringVar()
+    entid = tk.Entry(page, textvariable=id,font=(",''20',") , width=25)
+
+    entid.grid(row=1, column=2)
+    global status
+    status = tk.StringVar()
+    statusLabel = tk.Label(page, text='', font=15, fg='red', textvariable=status, width=10)
+    statusLabel.grid(row=2, column=1, columnspan=2)
+    tk.Button(page, text="返回", font=20, width=6, command=lambda :cancel(root)).grid(row=4, column=2, pady=10,sticky=tk.N)
+    tk.Button(page, text="查询", font=20, width=6, command=lambda :ok(id,root,page)).grid(row=4, column=1, pady=10, sticky=tk.N)
+    page.mainloop()
+
+def ok(id,root,page):
+    login(id,root,page)
+def login(ids,root,page):
+    id = ids.get()
+    if id :
+        validate = check(id)
+        if len(validate) != 0:
+            status.set("查找成功")
+            print(validate)
+            setup_UI2(validate,root,page)
         else:
-            self.status.set("请输入id号")
+            status.set('学号不存在')
+    else:
+        status.set("请输入id号")
 
 
 
-    def setup_UI2(self,student:Stu) :
-        self.geometry("500x700+500+100")
-        stu = Stu()
-        page = tk.Frame(self)
-        page.pack()
-        self.stringval =[]
-        print(student)
-        tk.Label(page, text='学生信息 ', font=30, width=10, pady=15, padx=15).grid(row=1, column=1, columnspan=5)
-        self.stringval.append(tk.StringVar())
-        tk.Label(page, text='学号： ', font=20, width=15, pady=15).grid(row=2, column=1)
-        tk.Entry(page,textvariable=self.stringval[0], font=(",''20',") ,width=10).grid(row=2, column=2)
-        self.stringval[0].set(f'{student[0].StuId}')
+def setup_UI2(student,root,page) :
+    root.geometry("500x700+500+100")
+    stu = Stu()
+    page = tk.Frame(root)
+    page.pack()
+    stringval =[]
+    print(student)
+    tk.Label(page, text='学生信息 ', font=30, width=10, pady=15, padx=15).grid(row=1, column=1, columnspan=5)
+    stringval.append(tk.StringVar())
+    tk.Label(page, text='学号： ', font=20, width=15, pady=15).grid(row=2, column=1)
+    tk.Entry(page,textvariable=stringval[0], font=(",''20',") ,width=10).grid(row=2, column=2)
+    stringval[0].set(f'{student[0].StuId}')
 
-        self.stringval.append(tk.StringVar())
-        tk.Label(page, text='姓名: ', font=20, width=15, pady=15).grid(row=3, column=1)
-        tk.Entry(page, textvariable=self.stringval[1], font=20, width=10).grid(row=3, column=2)
-        self.stringval[1].set(student[0].Name)
+    stringval.append(tk.StringVar())
+    tk.Label(page, text='姓名: ', font=20, width=15, pady=15).grid(row=3, column=1)
+    tk.Entry(page, textvariable=stringval[1], font=20, width=10).grid(row=3, column=2)
+    stringval[1].set(student[0].Name)
 
-        self.stringval.append(tk.StringVar())
-        tk.Label(page, text='性别: ', font=20, width=15, pady=15).grid(row=4, column=1)
-        tk.Entry(page, textvariable=self.stringval[2], font=20, width=10).grid(row=4, column=2)
-        self.stringval[2].set(student[0].Sex)
+    stringval.append(tk.StringVar())
+    tk.Label(page, text='性别: ', font=20, width=15, pady=15).grid(row=4, column=1)
+    tk.Entry(page, textvariable=stringval[2], font=20, width=10).grid(row=4, column=2)
+    stringval[2].set(student[0].Sex)
 
-        self.stringval.append(tk.StringVar())
-        tk.Label(page, text='班级: ', font=20, width=15, pady=15).grid(row=5, column=1)
-        tk.Entry(page, textvariable=self.stringval[3], font=20, width=10).grid(row=5, column=2)
-        self.stringval[3].set(student[0].ClassName)
+    stringval.append(tk.StringVar())
+    tk.Label(page, text='班级: ', font=20, width=15, pady=15).grid(row=5, column=1)
+    tk.Entry(page, textvariable=stringval[3], font=20, width=10).grid(row=5, column=2)
+    stringval[3].set(student[0].ClassName)
 
-        self.stringval.append(tk.StringVar())
-        tk.Label(page, text='数学: ', font=20, width=15, pady=15).grid(row=6, column=1)
-        tk.Entry(page, textvariable=self.stringval[4], font=20, width=10).grid(row=6, column=2)
-        self.stringval[4].set(student[0].Math)
+    stringval.append(tk.StringVar())
+    tk.Label(page, text='数学: ', font=20, width=15, pady=15).grid(row=6, column=1)
+    tk.Entry(page, textvariable=stringval[4], font=20, width=10).grid(row=6, column=2)
+    stringval[4].set(student[0].Math)
 
-        self.stringval.append(tk.StringVar())
-        tk.Label(page, text='语文: ', font=20, width=15, pady=15).grid(row=7, column=1)
-        tk.Entry(page, textvariable=self.stringval[5], font=20, width=10).grid(row=7, column=2)
-        self.stringval[5].set(student[0].Chinese)
+    stringval.append(tk.StringVar())
+    tk.Label(page, text='语文: ', font=20, width=15, pady=15).grid(row=7, column=1)
+    tk.Entry(page, textvariable=stringval[5], font=20, width=10).grid(row=7, column=2)
+    stringval[5].set(student[0].Chinese)
 
-        self.stringval.append(tk.StringVar())
-        tk.Label(page, text='英语: ', font=20, width=15, pady=15).grid(row=8, column=1)
-        tk.Entry(page, textvariable=self.stringval[6], font=20, width=10).grid(row=8, column=2)
-        self.stringval[6].set(student[0].English)
+    stringval.append(tk.StringVar())
+    tk.Label(page, text='英语: ', font=20, width=15, pady=15).grid(row=8, column=1)
+    tk.Entry(page, textvariable=stringval[6], font=20, width=10).grid(row=8, column=2)
+    stringval[6].set(student[0].English)
 
-        tk.Button(page, text="修改", font=20, width=6, command=self.updates).grid(row=9, column=1, pady=10)
-        tk.Button(page, text="返回", font=20, width=6, command=self.returns).grid(row=9, column=2, pady=10)
+    tk.Button(page, text="修改", font=20, width=6, command=lambda :updates(stringval,root,page)).grid(row=9, column=1, pady=10)
+    tk.Button(page, text="返回", font=20, width=6, command=lambda :returns(root)).grid(row=9, column=2, pady=10)
+    page.mainloop()
 
-
-    def updates(self):
-        stu = Stu()
-        stu.StuId = self.stringval[0].get()
-        stu.Name = self.stringval[1].get()
-        stu.Sex = self.stringval[2].get()
-        stu.ClassName = self.stringval[3].get()
-        stu.Math = self.stringval[4].get()
-        stu.Chinese = self.stringval[5].get()
-        stu.English = self.stringval[6].get()
-        print(stu)
-        dao.update(stu.StuId,stu)
-        showinfo("提示框","保存成功")
-        self.destroy()
-
-
-    def returns(self):
-        self.destroy()
-        Login()
-
-    def cancel(self):
-        self.destroy()
-        pass
+def updates(stringval,root,page):
+    stu = Stu()
+    stu.StuId = stringval[0].get()
+    stu.Name = stringval[1].get()
+    stu.Sex = stringval[2].get()
+    stu.ClassName = stringval[3].get()
+    stu.Math = stringval[4].get()
+    stu.Chinese = stringval[5].get()
+    stu.English = stringval[6].get()
+    print(stu)
+    dao.update(stu.StuId,stu)
+    showinfo("提示框","保存成功")
+    root.destroy()
 
 
+def returns(root):
+    root.destroy()
 
-if __name__ == '__main__':
-    Login().mainloop()
+def cancel(root):
+    root.destroy()
+    pass
+
+
+
+def up():
+    root = tk.Tk()
+    page = tk.Frame(root)
+    function(root,page)
